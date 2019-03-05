d308b0fec84e8efa4c9374c06847be9c5242e348
Tim <teenyoung42@gmail.com>
2019-03-06 11:23:18 +1300

    Find all matched pieces
    // Comment out some codes so the board is created with possible initial matches
    // Check if the piece at (x,y) is in a match, return all matched pieces.
    // Check all matches in the board, return a list of all matched pieces.
    // Prevent duplication
    // Show counts in Debug.Log

--- Assets/Scripts/BoardController.cs
+++ Assets/Scripts/BoardController.cs
@@ -15,15 +15,17 @@ public class BoardController : MonoBehaviour
     void Start()
     {
         SetupBoard();
+        List<GameObject> allMatchedPieces = CheckAllMatches();
+        Debug.Log("total:"+allMatchedPieces.Count);
     }
 
-    // Create a board without any initial match
+    // Create a board (without any initial match)
     void SetupBoard()
     {
         // Create a 2D array to store pieces and determine its size.
         pieces = new GameObject[boardWidth, boardHeight];
 
-        //Layout the board
+        // Layout the board
         for (int i = 0; i < boardWidth; i++)
         {
             for (int j = 0; j < boardHeight; j++)
@@ -33,11 +35,11 @@ public class BoardController : MonoBehaviour
                 // Pick a random piece prefab
                 GameObject randomPiecePrefab = piecePrefabs[Random.Range(0, piecePrefabs.Length)];
 
-                // If the current prefab will result in an initial match, repeat the random process until it will not.
-                while (CheckInitialMatch(i, j, randomPiecePrefab))
-                {
-                    randomPiecePrefab = piecePrefabs[Random.Range(0, piecePrefabs.Length)];
-                };
+                //// If the current prefab will result in an initial match, repeat the random process until it will not.
+                //while (CheckInitialMatch(i, j, randomPiecePrefab))
+                //{
+                //    randomPiecePrefab = piecePrefabs[Random.Range(0, piecePrefabs.Length)];
+                //};
 
                 // Instantiate the piece prefab as a child of the board. 
                 GameObject piece = Instantiate(randomPiecePrefab, coordinate, Quaternion.identity, this.transform);
@@ -51,13 +53,37 @@ public class BoardController : MonoBehaviour
         }
     }
 
+    // Check all matches in the board, return a list of all matched pieces.
+    List<GameObject> CheckAllMatches()
+    {
+        List<GameObject> allMatchedPieces = new List<GameObject>();
+
+        for (int i = 0; i < boardWidth; i++)
+        {
+            for (int j = 0; j < boardHeight; j++)
+            {
+                List<GameObject> piecesToAdd = CheckMatch(i, j);
+
+                // Prevent duplication
+                foreach (GameObject piece in piecesToAdd)
+                {
+                    if (!allMatchedPieces.Contains(piece))
+                    {
+                        allMatchedPieces.Add(piece);
+                    }
+                }
+            }
+        }
+
+        return allMatchedPieces;
+    }
+
     // Check if the prefab will result in an initial match should it be instantiated at (x,y).
     bool CheckInitialMatch(int x, int y, GameObject piecePrefab)
     {
-        // Check columns first for efficiency
+        // Check columns
         if (y > 1)
         {
-            // Every piece prefab has its unique tag
             if (pieces[x, y - 1].tag == piecePrefab.tag && pieces[x, y - 2].tag == piecePrefab.tag)
             {
                 return true;
@@ -76,4 +102,32 @@ public class BoardController : MonoBehaviour
 
         return false;
     }
+
+    // Check if the piece at (x,y) is in a match, return all matched pieces.
+    List<GameObject> CheckMatch(int x, int y)
+    {
+        List<GameObject> matchedPieces = new List<GameObject>();
+
+        //Check column match upwards
+        if (y < boardHeight - 2)
+        {
+            if (pieces[x, y].tag == pieces[x, y + 1].tag && pieces[x, y].tag == pieces[x, y + 2].tag)
+            {
+                matchedPieces.AddRange(new GameObject[] { pieces[x, y], pieces[x, y + 1], pieces[x, y + 2] });
+            }
+        }
+
+        //Check row match rightwards
+        if (x < boardWidth - 2)
+        {
+            if (pieces[x, y].tag == pieces[x + 1, y].tag && pieces[x, y].tag == pieces[x +2 , y].tag)
+            {
+                matchedPieces.AddRange(new GameObject[] { pieces[x, y], pieces[x +1, y], pieces[x + 2, y] });
+            }
+        }
+
+        return matchedPieces;
+    }
+
+    // Check if a piece will result in a match should it be moved into tile at (x,y), return all matched pieces
 }
