f53820477cb8031237d2c76a129dca9d58388042
Tim <teenyoung42@gmail.com>
2019-03-06 14:35:09 +1300

    Update BoardController.cs
    // Uncomment codes to eliminate initial matches again
    // CheckMatch method will check all four directions if required.
    // CheckMatch method can check matches in designated 2D array of pieces now
    // Simulate a swap of two pieces and create an alternative board in memory
    // Check matches in alternative boards to find potential matches in the real board

--- Assets/Scripts/BoardController.cs
+++ Assets/Scripts/BoardController.cs
@@ -15,8 +15,13 @@ public class BoardController : MonoBehaviour
     void Start()
     {
         SetupBoard();
+
+        // For test and debug uses only
         List<GameObject> allMatchedPieces = CheckAllMatches();
-        Debug.Log("total:"+allMatchedPieces.Count);
+        Debug.Log("Macthed pieces count:" + allMatchedPieces.Count);
+
+        List<GameObject> hintPieces = CheckPotentialMatch(2, 2);
+        Debug.Log("Hint:" + hintPieces.Count);
     }
 
     // Create a board (without any initial match)
@@ -35,11 +40,11 @@ public class BoardController : MonoBehaviour
                 // Pick a random piece prefab
                 GameObject randomPiecePrefab = piecePrefabs[Random.Range(0, piecePrefabs.Length)];
 
-                //// If the current prefab will result in an initial match, repeat the random process until it will not.
-                //while (CheckInitialMatch(i, j, randomPiecePrefab))
-                //{
-                //    randomPiecePrefab = piecePrefabs[Random.Range(0, piecePrefabs.Length)];
-                //};
+                // If the current prefab will result in an initial match, repeat the random process until it will not.
+                while (CheckInitialMatch(i, j, randomPiecePrefab))
+                {
+                    randomPiecePrefab = piecePrefabs[Random.Range(0, piecePrefabs.Length)];
+                };
 
                 // Instantiate the piece prefab as a child of the board. 
                 GameObject piece = Instantiate(randomPiecePrefab, coordinate, Quaternion.identity, this.transform);
@@ -53,7 +58,7 @@ public class BoardController : MonoBehaviour
         }
     }
 
-    // Check all matches in the board, return a list of all matched pieces.
+    // Check all matches in the board, return a list of all matched pieces on the board.
     List<GameObject> CheckAllMatches()
     {
         List<GameObject> allMatchedPieces = new List<GameObject>();
@@ -62,7 +67,7 @@ public class BoardController : MonoBehaviour
         {
             for (int j = 0; j < boardHeight; j++)
             {
-                List<GameObject> piecesToAdd = CheckMatch(i, j);
+                List<GameObject> piecesToAdd = CheckMatch(i, j, pieces);
 
                 // Prevent duplication
                 foreach (GameObject piece in piecesToAdd)
@@ -103,8 +108,8 @@ public class BoardController : MonoBehaviour
         return false;
     }
 
-    // Check if the piece at (x,y) is in a match, return all matched pieces.
-    List<GameObject> CheckMatch(int x, int y)
+    // Check if the piece at (x,y) is in a match, return all matched pieces of this match. Only check upwards and rightwards if fourDirection is false.
+    List<GameObject> CheckMatch(int x, int y, GameObject[,] pieces, bool fourDirections = false)
     {
         List<GameObject> matchedPieces = new List<GameObject>();
 
@@ -120,14 +125,136 @@ public class BoardController : MonoBehaviour
         //Check row match rightwards
         if (x < boardWidth - 2)
         {
-            if (pieces[x, y].tag == pieces[x + 1, y].tag && pieces[x, y].tag == pieces[x +2 , y].tag)
+            if (pieces[x, y].tag == pieces[x + 1, y].tag && pieces[x, y].tag == pieces[x + 2, y].tag)
             {
-                matchedPieces.AddRange(new GameObject[] { pieces[x, y], pieces[x +1, y], pieces[x + 2, y] });
+                matchedPieces.AddRange(new GameObject[] { pieces[x, y], pieces[x + 1, y], pieces[x + 2, y] });
             }
         }
+
+        // Check other two directions if required.
+        if (fourDirections)
+        {
+            //Check column match downwards
+            if (y > 1)
+            {
+                if (pieces[x, y].tag == pieces[x, y - 1].tag && pieces[x, y].tag == pieces[x, y - 2].tag)
+                {
+                    matchedPieces.AddRange(new GameObject[] { pieces[x, y], pieces[x, y - 1], pieces[x, y - 2] });
+                }
+            }
+
+            //Check row match leftwards
+            if (x > 1)
+            {
+                if (pieces[x, y].tag == pieces[x - 1, y].tag && pieces[x, y].tag == pieces[x - 2, y].tag)
+                {
+                    matchedPieces.AddRange(new GameObject[] { pieces[x, y], pieces[x - 1, y], pieces[x - 2, y] });
+                }
+            }
+        }
+
 
         return matchedPieces;
     }
 
-    // Check if a piece will result in a match should it be moved into tile at (x,y), return all matched pieces
+    // Simulate upwards and rightwards swaps start from (x,y), if it will result in a match return all pieces of the first potential match
+    List<GameObject> CheckPotentialMatch(int x, int y)
+    {
+        List<GameObject> potentialMatchedPieces = new List<GameObject>();
+
+        // Check if upwards swap is possible and necessary
+        if (y < boardHeight-1)
+        {
+            if (pieces[x, y].tag != pieces[x, y + 1].tag)
+            {
+                // Simulate upwards swap
+                GameObject[,] piecesAfterSwapUp = (GameObject[,])pieces.Clone();
+                piecesAfterSwapUp[x, y] = pieces[x, y + 1];
+                piecesAfterSwapUp[x, y + 1] = pieces[x, y];
+
+                // Check matches at four directions on the 1st swaped piece
+                List<GameObject> piecesToAdd = CheckMatch(x, y, piecesAfterSwapUp, true);
+                if (piecesToAdd.Count != 0)
+                {
+                    // Prevent duplication
+                    foreach (GameObject piece in piecesToAdd)
+                    {
+                        if (!potentialMatchedPieces.Contains(piece))
+                        {
+                            potentialMatchedPieces.Add(piece);
+                        }
+                    }
+
+                    return potentialMatchedPieces;
+                }
+                else
+                {
+                    // Check matches at four directions on the 2nd swaped piece
+                    piecesToAdd = CheckMatch(x, y + 1, piecesAfterSwapUp, true);
+                    if (piecesToAdd.Count != 0)
+                    {
+                        // Prevent duplication
+                        foreach (GameObject piece in piecesToAdd)
+                        {
+                            if (!potentialMatchedPieces.Contains(piece))
+                            {
+                                potentialMatchedPieces.Add(piece);
+                            }
+                        }
+
+                        return potentialMatchedPieces;
+                    }
+                }
+            }
+        }
+
+
+        // Check if rightwards swap is possible and necessary
+        if (x < boardWidth - 1)
+        {
+            if (pieces[x, y].tag != pieces[x + 1, y].tag)
+            {
+                // Simulate rightwards swap
+                GameObject[,] piecesAfterSwapRight = (GameObject[,])pieces.Clone();
+                piecesAfterSwapRight[x, y] = pieces[x + 1, y];
+                piecesAfterSwapRight[x + 1, y] = pieces[x, y];
+
+                // Check matches at four directions on the 1st swaped piece
+                List<GameObject> piecesToAdd = CheckMatch(x, y, piecesAfterSwapRight, true);
+                if (piecesToAdd.Count != 0)
+                {
+                    // Prevent duplication
+                    foreach (GameObject piece in piecesToAdd)
+                    {
+                        if (!potentialMatchedPieces.Contains(piece))
+                        {
+                            potentialMatchedPieces.Add(piece);
+                        }
+                    }
+
+                    return potentialMatchedPieces;
+                }
+                else
+                {
+                    // Check matches at four directions on the 2nd swaped piece
+                    piecesToAdd = CheckMatch(x + 1, y, piecesAfterSwapRight, true);
+                    if (piecesToAdd.Count != 0)
+                    {
+                        // Prevent duplication
+                        foreach (GameObject piece in piecesToAdd)
+                        {
+                            if (!potentialMatchedPieces.Contains(piece))
+                            {
+                                potentialMatchedPieces.Add(piece);
+                            }
+                        }
+
+                        return potentialMatchedPieces;
+                    }
+                }
+            }
+        }
+
+        return potentialMatchedPieces;
+    }
 }
