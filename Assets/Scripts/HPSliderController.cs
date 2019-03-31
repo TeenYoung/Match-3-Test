9d3b0b6b97fcade29712b46e6c4fbbfa34642822
Tim <teenyoung42@gmail.com>
2019-03-31 14:05:57 +1300

    HP Sliders
    //Add HP sliders to play and monsters
    //Add HPSliderController to update value and color for hp sliders

---
+++ Assets/Scripts/HPSliderController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPSliderController : MonoBehaviour
+{
+    public Slider hpSlider;
+    public Image fillImage;
+    public Color fullHpColor, zeroHpColor;
+
+    public float MaxHp { get; set; }
+    public float CurrentHp { get; set; }
+
+    //// Tester
+    //private void Start()
+    //{
+    //    MaxHp = 100f;
+    //    CurrentHp = 30f;
+
+    //    hpSlider.maxValue = MaxHp;
+    //    SetHpSlider();
+    //}
+
+    // Update the value and color of the Hp Slider
+    private void SetHpSlider()
+    {
+        hpSlider.value = CurrentHp;
+
+        fillImage.color = Color.Lerp(zeroHpColor, fullHpColor, CurrentHp / MaxHp);
+    }
+}
