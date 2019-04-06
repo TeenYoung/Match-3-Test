710b4f1c19e7fce9f37eecd8a9036db37538c199
Tim <teenyoung42@gmail.com>
2019-04-07 10:35:11 +1200

    Adjust camera orthographic size in some rare screen resolutions

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Camera mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Screen.currentResolution.height/ Screen.currentResolution.width > 2)
+        {
+            mainCamera.orthographicSize = 7;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
