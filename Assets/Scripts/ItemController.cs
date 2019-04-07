4f7993e4cbad4470f3b7b2a388fe419a8f547216
Tim <teenyoung42@gmail.com>
2019-04-07 13:37:33 +1200

    Dynamic UI for Items
    Add an item UI prefab
    Add a script "ItemController" on ItemArea
    // Instantiate item UI prefab in ItemController

---
+++ Assets/Scripts/ItemController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemController : MonoBehaviour
+{
+    public GameObject itemPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject item1 = Instantiate(itemPrefab,transform);
+        GameObject item2 = Instantiate(itemPrefab, transform);
+        GameObject item3 = Instantiate(itemPrefab, transform);
+    }
+}
