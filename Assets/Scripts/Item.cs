d654ac89f7efb8d021e6740edec7dffd99a0d944
Tim <teenyoung42@gmail.com>
2019-04-14 10:13:43 +1200

    Item Class
    //Add item class
    //Select item on click

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+
+    private int recoverHP, recoverTurns;
+
+    private ItemController items;
+
+    private void Start()
+    {
+        items = transform.GetComponentInParent<ItemController>();
+    }
+
+    public void SetItem(int recoverHP, int recoverTurns)
+    {
+        this.recoverHP = recoverHP;
+        this.recoverTurns = recoverTurns;
+    }
+
+    public void OnClick()
+    {
+        items.Select(this);
+    }
+}
--- Assets/Scripts/ItemController.cs
+++ Assets/Scripts/ItemController.cs
@@ -5,12 +5,32 @@ using UnityEngine;
 public class ItemController : MonoBehaviour
 {
     public GameObject itemPrefab;
+    public RectTransform selectFrameRectTransform;
+
+    private Item selectItem;
 
     // Start is called before the first frame update
     void Start()
     {
-        GameObject item1 = Instantiate(itemPrefab,transform);
-        GameObject item2 = Instantiate(itemPrefab, transform);
-        GameObject item3 = Instantiate(itemPrefab, transform);
+        Item item1 = Instantiate(itemPrefab, transform).GetComponent<Item>();
+        item1.name = "Herb";
+        item1.SetItem(30,0);
+
+        Item item2 = Instantiate(itemPrefab, transform).GetComponent<Item>();
+        item2.name = "HealPotion";
+        item2.SetItem(20, 3);
+
+        Item item3 = Instantiate(itemPrefab, transform).GetComponent<Item>();
+        item3.name = "QuickHealPotion";
+        item3.SetItem(60, 0);
+    }
+
+    public void Select(Item item)
+    {
+        selectItem = item;
+
+        // Keep y unchaned wile move select frame horizontally
+        float y = selectFrameRectTransform.anchoredPosition.y;
+        selectFrameRectTransform.anchoredPosition = new Vector2(selectItem.GetComponent<RectTransform>().anchoredPosition.x, y);
     }
 }
