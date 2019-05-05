284a2f5732e09fe88636ab8785a9602a9fa5fd3d
Ya <yasmine.WenY@gmail.com>
2019-05-06 09:40:41 +1200

    Week 3
    1. CHANGE some variable names

--- Assets/Scripts/Buff.cs
+++ Assets/Scripts/Buff.cs
@@ -16,13 +16,13 @@ using UnityEngine.UI;
 
 public class Buff : MonoBehaviour
 {   
-    public Text BuffText;
+    public Text buffText;
     public string buffName;
     public Creature creature;
 
     public int BuffNum { get; set; }
 
-    float bleedingDMGRate;
+    float bleedingDMGBase;
     float healHP;
        
     /// <summary>
@@ -34,17 +34,17 @@ public class Buff : MonoBehaviour
     /// <param name="creature"></param>
     /// <param name="color"></param>
     /// <param name="tagName">tagName: TriggerAt_TurnBegin, TriggerAt_TurnEnd, TriggerAt_Move, TriggerAt_Attack, TriggerAt_TakeDMG...</param>
-    /// <param name="bleedingDMGRate"></param>
-    /// <param name="dodgeRate"></param>
+    /// <param name="bleedingDMGBase"></param>
+    /// <param name="dodgeRateBase"></param>
     ////public void Initialize(BuffType type, int remainTurn, Transform buffZone, Creature creature, Image image, string tagName,float bleedingDMGRate, float dodgeRate
-    public void Initialize(string buffName, int remainTurn, Transform buffZone, Creature creature, Color color, string tagName,float bleedingDMGRate, float dodgeRate,float healHP, int chargeLayer)
+    public void Initialize(string buffName, int remainTurn, Transform buffZone, Creature creature, Color color, string tagName,float bleedingDMGBase, float dodgeRateBase,float healHP, int chargeLayer)
     {
         this.buffName = buffName;
 
         //buff num depends on buff type
         if (this.buffName == "Dodge") //show dodge rate
         {
-            this.BuffNum = System.Convert.ToInt32(dodgeRate);
+            this.BuffNum = System.Convert.ToInt32(dodgeRateBase);
             //Debug.Log("Add dodge, rate = " + BuffNum);
         }
         else if (this.buffName == "Charge")
@@ -60,9 +60,9 @@ public class Buff : MonoBehaviour
         this.gameObject.tag = tagName;
         //this.gameObject.GetComponent<Image>().sprite = Resources.Load<Sprite>(imgPath) as Sprite; //need to re-edit
 
-        this.bleedingDMGRate = bleedingDMGRate;        
+        this.bleedingDMGBase = bleedingDMGBase;        
         this.creature.ChargeLayer = chargeLayer;
-        this.creature.DodgeRate = dodgeRate;
+        this.creature.DodgeRate = dodgeRateBase;
         this.healHP = healHP;
         this.gameObject.transform.SetParent(buffZone);
         UpdateBuffTurnText();
@@ -70,15 +70,15 @@ public class Buff : MonoBehaviour
 
     public void UpdateBuffTurnText()
     {
-        BuffText.text = this.BuffNum.ToString();
+        buffText.text = this.BuffNum.ToString();
     }
 
     public void Trigger()
     {
-        if (bleedingDMGRate != 0)
+        if (bleedingDMGBase != 0)
         {            
-            Debug.Log(creature.name + buffName + " dmg " + BuffNum * bleedingDMGRate);
-            creature.TakeDMG(BuffNum * bleedingDMGRate);
+            Debug.Log(creature.name + buffName + " dmg " + BuffNum * bleedingDMGBase);
+            creature.TakeDMG(BuffNum * bleedingDMGBase);
             this.BuffNum--;
             UpdateBuffTurnText();
             ////destroy the object if remainturn =0
